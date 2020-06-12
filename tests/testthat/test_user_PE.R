fd956a89051ddceef2b62693ffb32f65008ae259
Vittorio Orlandi <vittorio.d.orlandi@gmail.com>
2020-06-12 10:59:28 -0400

    changed unit test that depended on external package

--- tests/testthat/test_user_PE.R
+++ tests/testthat/test_user_PE.R
@@ -1,13 +1,13 @@
 data <- gen_data(500)
 holdout <- gen_data(500)
 
-test_that("can use BART to predict", {
+test_that("user specified PE methods work", {
 
-  my_fit <- dbarts::bart
-  my_fit_params <- list(ntree = 100, verbose = FALSE, keeptrees = TRUE)
-  my_predict <- function(bart_fit, new_data) {
-    return(colMeans(predict(bart_fit, new_data)))
-  }
+  my_fit <- glmnet::cv.glmnet
+  my_fit_params <- list(nfolds = 5)
+  my_predict <- predict
+  my_predict_params <- list(s = "lambda.min")
+
   FLAME_out <-
     FLAME(data = data, holdout = holdout,
           user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
