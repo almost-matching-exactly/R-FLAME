9765412723a8b7b82ea90e4e79a4a1cf5e7dd3b7
Vittorio Orlandi <52432620+vittorioorlandi@users.noreply.github.com>
2021-07-01 20:43:16 -0400

    tests to make sure that new treatment of rownames doesn't break anything

--- tests/testthat/test_data_splitting.R
+++ tests/testthat/test_data_splitting.R
@@ -3,18 +3,61 @@ p <- 5
 data <- gen_data(5 * n, p)
 weights <- runif(p)
 
-test_that("data splitting doesn't affect post matching", {
-  flout <- FLAME(data, weights = weights)
-  MGs <- MG(1:nrow(flout$data), flout)
-  expect_true(TRUE)
+test_that("pre-splitting data doesn't affect post matching", {
 
-  holdout_inds <- sample(1:nrow(data), round(0.1 * nrow(data)), TRUE)
+  holdout_inds <- sample(seq_len(nrow(data)), round(0.1 * nrow(data)), TRUE)
   holdout <- data[holdout_inds, ]
   data <- data[-holdout_inds, ]
 
   flout <- FLAME(data, holdout, weights = weights)
-  MGs <- MG(1:nrow(flout$data), flout)
-  expect_true(TRUE)
+  for (i in seq_len(nrow(flout$data))) {
+    i <- as.numeric(i)
+    inds <- flout$MGs[[i]]
+    tmp <-
+      MG(as.numeric(rownames(flout$data))[i], flout, index_only = TRUE)[[1]]
+    if (is.null(inds)) {
+      expect_null(tmp)
+    }
+    else {
+      expect_equivalent(sort(inds), sort(tmp))
+    }
+  }
+})
+
+test_that("auto-splitting data doesn't affect post matching", {
+  flout <- FLAME(data, weights = weights)
+  for (i in seq_len(nrow(flout$data))) {
+    i <- as.numeric(i)
+    inds <- flout$MGs[[i]]
+    tmp <-
+      MG(as.numeric(rownames(flout$data))[i], flout, index_only = TRUE)[[1]]
+    if (is.null(inds)) {
+      expect_null(tmp)
+    }
+    else {
+      expect_equivalent(sort(inds), sort(tmp))
+    }
+  }
+})
+
+test_that("scrambled rownames and auto-splitting data
+          doesn't affect post matching", {
+
+  rownames(data) <- sample(nrow(data))
+
+  flout <- FLAME(data, weights = weights)
+  for (i in seq_len(nrow(flout$data))) {
+    i <- as.numeric(i)
+    inds <- flout$MGs[[i]]
+    tmp <-
+      MG(as.numeric(rownames(flout$data))[i], flout, index_only = TRUE)[[1]]
+    if (is.null(inds)) {
+      expect_null(tmp)
+    }
+    else {
+      expect_equivalent(sort(inds), sort(tmp))
+    }
+  }
 })
 
 test_that("can't require data splitting without outcome", {
