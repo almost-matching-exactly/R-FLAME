020b33e9cafccd572499f4008e0bee39db1808ab
Vittorio Orlandi <52432620+vittorioorlandi@users.noreply.github.com>
2021-07-06 12:32:00 -0400

    skip PE tests on CRAN (they are time intensive)

--- tests/testthat/test_PE.R
+++ tests/testthat/test_PE.R
@@ -1,99 +1,109 @@
-n <- 500
-p <- 4
-data <- gen_data(n, p)
-holdout <- gen_data(n, p)
+# Thanks to Dirk Eddelbuettel:
+#  https://stackoverflow.com/questions/36166288/skip-tests-on-cran-but-run-locally
+if (length(strsplit(packageDescription("FLAME")$Version, "\\.")[[1]]) > 3) {
+  skipping_on_cran <- FALSE
+} else {
+  skipping_on_cran <- TRUE
+}
 
-test_that("ridge works with continuous outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'ridge')
-  expect_true(TRUE)
-})
+if (!skipping_on_cran) {
+  n <- 500
+  p <- 4
+  data <- gen_data(n, p)
+  holdout <- gen_data(n, p)
 
-test_that("XGBoost works with continuous outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'xgb')
-  expect_true(TRUE)
-})
+  test_that("ridge works with continuous outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'ridge')
+    expect_true(TRUE)
+  })
 
-data$outcome <-
-  exp(scale(data$outcome, scale = F)) /
-  (1 + exp(scale(data$outcome, scale = F)))
+  test_that("XGBoost works with continuous outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'xgb')
+    expect_true(TRUE)
+  })
 
-data$outcome <- round(data$outcome)
+  data$outcome <-
+    exp(scale(data$outcome, scale = F)) /
+    (1 + exp(scale(data$outcome, scale = F)))
 
-holdout$outcome <-
-  exp(scale(holdout$outcome, scale = F)) /
-  (1 + exp(scale(holdout$outcome, scale = F)))
+  data$outcome <- round(data$outcome)
 
-holdout$outcome <- round(holdout$outcome)
+  holdout$outcome <-
+    exp(scale(holdout$outcome, scale = F)) /
+    (1 + exp(scale(holdout$outcome, scale = F)))
 
-test_that("ridge works with binary continuous outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'ridge')
-  expect_true(TRUE)
-})
+  holdout$outcome <- round(holdout$outcome)
 
-test_that("XGBoost works with binary continuous outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'xgb')
-  expect_true(TRUE)
-})
+  test_that("ridge works with binary continuous outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'ridge')
+    expect_true(TRUE)
+  })
 
-data$outcome <- factor(data$outcome)
-holdout$outcome <- factor(holdout$outcome)
+  test_that("XGBoost works with binary continuous outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'xgb')
+    expect_true(TRUE)
+  })
 
-test_that("ridge works with binary factor outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'ridge')
-  expect_null(flout$CATE)
-})
+  data$outcome <- factor(data$outcome)
+  holdout$outcome <- factor(holdout$outcome)
 
-test_that("XGBoost works with binary factor outcome", {
-  flout <- FLAME(data, holdout, PE_method = 'xgb')
-  expect_null(flout$CATE)
-})
+  test_that("ridge works with binary factor outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'ridge')
+    expect_null(flout$CATE)
+  })
 
-data$outcome <- factor(sample(c('Green', 'White', 'Red'), n, TRUE))
-holdout$outcome <- factor(sample(c('Green', 'White', 'Red'), n, TRUE))
+  test_that("XGBoost works with binary factor outcome", {
+    flout <- FLAME(data, holdout, PE_method = 'xgb')
+    expect_null(flout$CATE)
+  })
 
-test_that("ridge works with multiclass outcomes", {
-  flout <- FLAME(data, holdout, PE_method = 'ridge')
-  expect_null(flout$CATE)
-})
+  data$outcome <- factor(sample(c('Green', 'White', 'Red'), n, TRUE))
+  holdout$outcome <- factor(sample(c('Green', 'White', 'Red'), n, TRUE))
 
-test_that("XGBoost works with multiclass outcomes", {
-  flout <- FLAME(data, holdout, PE_method = 'xgb')
-  expect_null(flout$CATE)
-})
+  test_that("ridge works with multiclass outcomes", {
+    flout <- FLAME(data, holdout, PE_method = 'ridge')
+    expect_null(flout$CATE)
+  })
 
-######
-# test_that("independent of outcome levels", {
-#   data <- gen_data(n, p)
-#   holdout <- gen_data(n, p)
-#
-#
-#   data$outcome <-
-#     exp(scale(data$outcome, scale = F)) /
-#     (1 + exp(scale(data$outcome, scale = F)))
-#
-#   data$outcome <- round(data$outcome)
-#
-#   holdout$outcome <-
-#     exp(scale(holdout$outcome, scale = F)) /
-#     (1 + exp(scale(holdout$outcome, scale = F)))
-#
-#   holdout$outcome <- round(holdout$outcome)
-#
-#   flout <- FLAME(data, holdout)
-#
-#   data$outcome <- factor(data$outcome)
-#   holdout$outcome <- factor(holdout$outcome)
-#
-#   flout_factor <- FLAME(data, holdout)
-#   if (identical(flout$dropped, flout_factor$dropped)) {
-#     expect_identical(flout$MGs, flout_factor$MGs)
-#   }
-#
-#   levels(data$outcome) <- c('A', 'B')
-#   levels(holdout$outcome) <- c('A', 'B')
-#
-#   flout_factor2 <- FLAME(data, holdout)
-#   if (identical(flout$dropped, flout_factor2$dropped)) {
-#     expect_identical(flout$MGs, flout_factor2$MGs)
-#   }
-# })
+  test_that("XGBoost works with multiclass outcomes", {
+    flout <- FLAME(data, holdout, PE_method = 'xgb')
+    expect_null(flout$CATE)
+  })
+
+  ######
+  # test_that("independent of outcome levels", {
+  #   data <- gen_data(n, p)
+  #   holdout <- gen_data(n, p)
+  #
+  #
+  #   data$outcome <-
+  #     exp(scale(data$outcome, scale = F)) /
+  #     (1 + exp(scale(data$outcome, scale = F)))
+  #
+  #   data$outcome <- round(data$outcome)
+  #
+  #   holdout$outcome <-
+  #     exp(scale(holdout$outcome, scale = F)) /
+  #     (1 + exp(scale(holdout$outcome, scale = F)))
+  #
+  #   holdout$outcome <- round(holdout$outcome)
+  #
+  #   flout <- FLAME(data, holdout)
+  #
+  #   data$outcome <- factor(data$outcome)
+  #   holdout$outcome <- factor(holdout$outcome)
+  #
+  #   flout_factor <- FLAME(data, holdout)
+  #   if (identical(flout$dropped, flout_factor$dropped)) {
+  #     expect_identical(flout$MGs, flout_factor$MGs)
+  #   }
+  #
+  #   levels(data$outcome) <- c('A', 'B')
+  #   levels(holdout$outcome) <- c('A', 'B')
+  #
+  #   flout_factor2 <- FLAME(data, holdout)
+  #   if (identical(flout$dropped, flout_factor2$dropped)) {
+  #     expect_identical(flout$MGs, flout_factor2$MGs)
+  #   }
+  # })
+}
--- tests/testthat/test_user_PE.R
+++ tests/testthat/test_user_PE.R
@@ -12,144 +12,155 @@
 #'   in-sample predictions, which, if the outcome is binary or multi-class, must
 #'   be maximum probability class labels. See below for examples.
 
-my_PE_cont <- function(X, Y) {
-  lm(Y ~ ., data = as.data.frame(cbind(X, Y = Y)))$fitted.values
+# Thanks to Dirk Eddelbuettel:
+#  https://stackoverflow.com/questions/36166288/skip-tests-on-cran-but-run-locally
+if (length(strsplit(packageDescription("FLAME")$Version, "\\.")[[1]]) > 3) {
+  skipping_on_cran <- FALSE
+} else {
+  skipping_on_cran <- TRUE
 }
 
-my_PE_bin <- function(X, Y) {
-  df <- as.data.frame(cbind(X, Y = Y))
-  X <- model.matrix(Y ~ ., data = df)
-  fit <- glmnet::glmnet(X, Y, family = 'binomial')
-  return(predict(fit, X, s = 0.01, type = 'class'))
+if (!skipping_on_cran) {
+
+  my_PE_cont <- function(X, Y) {
+    lm(Y ~ ., data = as.data.frame(cbind(X, Y = Y)))$fitted.values
+  }
+
+  my_PE_bin <- function(X, Y) {
+    df <- as.data.frame(cbind(X, Y = Y))
+    X <- model.matrix(Y ~ ., data = df)
+    fit <- glmnet::glmnet(X, Y, family = 'binomial')
+    return(predict(fit, X, s = 0.01, type = 'class'))
+  }
+
+  my_PE_multiclass <- function(X, Y) {
+    df <- as.data.frame(cbind(X, Y = Y))
+    X <- model.matrix(Y ~ ., data = df)
+    fit <- glmnet::glmnet(X, Y, family = 'multinomial')
+    return(predict(fit, X, s = 0.01, type = 'class'))
+  }
+
+  test_that("user continuous PE methods works", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    flout <- FLAME(data, holdout, PE_method = my_PE_cont)
+    expect_true(TRUE)
+  })
+
+  test_that("user binary factor PE methods works", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- cut(data$outcome, breaks = 2, labels = c('A', 'B'))
+    holdout$outcome <- cut(holdout$outcome, breaks = 2, labels = c('A', 'B'))
+    flout <- FLAME(data, holdout, PE_method = my_PE_bin)
+    expect_true(TRUE)
+  })
+
+  test_that("user binary numeric PE methods works", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- ifelse(data$outcome > median(data$outcome), 1, 0)
+    holdout$outcome <- ifelse(holdout$outcome > median(holdout$outcome), 1, 0)
+    flout <- FLAME(data, holdout, PE_method = my_PE_bin)
+    expect_true(TRUE)
+  })
+
+  test_that("user multiclass PE methods works", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- cut(data$outcome, breaks = 3, labels = c('A', 'B', 'C'))
+    holdout$outcome <- cut(holdout$outcome, breaks = 3, labels = c('A', 'B', 'C'))
+    flout <- FLAME(data, holdout, PE_method = my_PE_multiclass)
+    expect_true(TRUE)
+  })
+
+  test_that("old user continuous PE methods work", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+
+    my_fit <- glmnet::cv.glmnet
+    my_fit_params <- list(nfolds = 5)
+    my_predict <- predict
+    my_predict_params <- list(s = "lambda.min")
+
+    FLAME_out <-
+      FLAME(data = data, holdout = holdout,
+            user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
+            user_PE_predict = my_predict,
+            user_PE_predict_params = my_predict_params)
+
+    expect_true(TRUE)
+  })
+
+  test_that("old user binary numeric PE methods work", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- ifelse(data$outcome > median(data$outcome), 1, 0)
+    holdout$outcome <- ifelse(holdout$outcome > median(holdout$outcome), 1, 0)
+    my_fit <- glmnet::cv.glmnet
+    my_fit_params <- list(nfolds = 5, family = 'binomial')
+    my_predict <- predict
+    my_predict_params <- list(s = "lambda.min", type = "class")
+
+    FLAME_out <-
+      FLAME(data = data, holdout = holdout,
+            user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
+            user_PE_predict = my_predict,
+            user_PE_predict_params = my_predict_params)
+
+    expect_true(TRUE)
+  })
+
+  test_that("old user binary factor PE methods work", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- cut(data$outcome, breaks = 2, labels = c('A', 'B'))
+    holdout$outcome <- cut(holdout$outcome, breaks = 2, labels = c('A', 'B'))
+    my_fit <- glmnet::cv.glmnet
+    my_fit_params <- list(nfolds = 5, family = 'binomial')
+    my_predict <- predict
+    my_predict_params <- list(s = "lambda.min", type = "class")
+
+    FLAME_out <-
+      FLAME(data = data, holdout = holdout,
+            user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
+            user_PE_predict = my_predict,
+            user_PE_predict_params = my_predict_params)
+
+    expect_true(TRUE)
+  })
+
+  test_that("old user multiclass PE methods work", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    data$outcome <- cut(data$outcome, breaks = 3, labels = c('A', 'B', 'C'))
+    holdout$outcome <- cut(holdout$outcome, breaks = 3, labels = c('A', 'B', 'C'))
+    my_fit <- glmnet::cv.glmnet
+    my_fit_params <- list(nfolds = 5, family = 'multinomial')
+    my_predict <- predict
+    my_predict_params <- list(s = "lambda.min", type = "class")
+
+    FLAME_out <-
+      FLAME(data = data, holdout = holdout,
+            user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
+            user_PE_predict = my_predict,
+            user_PE_predict_params = my_predict_params)
+
+    expect_true(TRUE)
+  })
+
+  test_that("old-new PE method conflicts are caught", {
+    data <- gen_data(500, 4)
+    holdout <- gen_data(500, 4)
+    my_fit <- glmnet::cv.glmnet
+    my_fit_params <- list(nfolds = 5)
+    my_predict <- predict
+    my_predict_params <- list(s = "lambda.min")
+
+    expect_error(FLAME(data = data, holdout = holdout,
+                       PE_method = my_PE_cont,
+                       user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
+                       user_PE_predict = my_predict,
+                       user_PE_predict_params = my_predict_params))
+  })
 }
-
-my_PE_multiclass <- function(X, Y) {
-  df <- as.data.frame(cbind(X, Y = Y))
-  X <- model.matrix(Y ~ ., data = df)
-  fit <- glmnet::glmnet(X, Y, family = 'multinomial')
-  return(predict(fit, X, s = 0.01, type = 'class'))
-}
-
-test_that("user continuous PE methods works", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  flout <- FLAME(data, holdout, PE_method = my_PE_cont)
-  expect_true(TRUE)
-})
-
-test_that("user binary factor PE methods works", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- cut(data$outcome, breaks = 2, labels = c('A', 'B'))
-  holdout$outcome <- cut(holdout$outcome, breaks = 2, labels = c('A', 'B'))
-  flout <- FLAME(data, holdout, PE_method = my_PE_bin)
-  expect_true(TRUE)
-})
-
-test_that("user binary numeric PE methods works", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- ifelse(data$outcome > median(data$outcome), 1, 0)
-  holdout$outcome <- ifelse(holdout$outcome > median(holdout$outcome), 1, 0)
-  flout <- FLAME(data, holdout, PE_method = my_PE_bin)
-  expect_true(TRUE)
-})
-
-test_that("user multiclass PE methods works", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- cut(data$outcome, breaks = 3, labels = c('A', 'B', 'C'))
-  holdout$outcome <- cut(holdout$outcome, breaks = 3, labels = c('A', 'B', 'C'))
-  flout <- FLAME(data, holdout, PE_method = my_PE_multiclass)
-  expect_true(TRUE)
-})
-
-test_that("old user continuous PE methods work", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-
-  my_fit <- glmnet::cv.glmnet
-  my_fit_params <- list(nfolds = 5)
-  my_predict <- predict
-  my_predict_params <- list(s = "lambda.min")
-
-  FLAME_out <-
-    FLAME(data = data, holdout = holdout,
-          user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
-          user_PE_predict = my_predict,
-          user_PE_predict_params = my_predict_params)
-
-  expect_true(TRUE)
-})
-
-test_that("old user binary numeric PE methods work", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- ifelse(data$outcome > median(data$outcome), 1, 0)
-  holdout$outcome <- ifelse(holdout$outcome > median(holdout$outcome), 1, 0)
-  my_fit <- glmnet::cv.glmnet
-  my_fit_params <- list(nfolds = 5, family = 'binomial')
-  my_predict <- predict
-  my_predict_params <- list(s = "lambda.min", type = "class")
-
-  FLAME_out <-
-    FLAME(data = data, holdout = holdout,
-          user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
-          user_PE_predict = my_predict,
-          user_PE_predict_params = my_predict_params)
-
-  expect_true(TRUE)
-})
-
-test_that("old user binary factor PE methods work", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- cut(data$outcome, breaks = 2, labels = c('A', 'B'))
-  holdout$outcome <- cut(holdout$outcome, breaks = 2, labels = c('A', 'B'))
-  my_fit <- glmnet::cv.glmnet
-  my_fit_params <- list(nfolds = 5, family = 'binomial')
-  my_predict <- predict
-  my_predict_params <- list(s = "lambda.min", type = "class")
-
-  FLAME_out <-
-    FLAME(data = data, holdout = holdout,
-          user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
-          user_PE_predict = my_predict,
-          user_PE_predict_params = my_predict_params)
-
-  expect_true(TRUE)
-})
-
-test_that("old user multiclass PE methods work", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  data$outcome <- cut(data$outcome, breaks = 3, labels = c('A', 'B', 'C'))
-  holdout$outcome <- cut(holdout$outcome, breaks = 3, labels = c('A', 'B', 'C'))
-  my_fit <- glmnet::cv.glmnet
-  my_fit_params <- list(nfolds = 5, family = 'multinomial')
-  my_predict <- predict
-  my_predict_params <- list(s = "lambda.min", type = "class")
-
-  FLAME_out <-
-    FLAME(data = data, holdout = holdout,
-          user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
-          user_PE_predict = my_predict,
-          user_PE_predict_params = my_predict_params)
-
-  expect_true(TRUE)
-})
-
-test_that("old-new PE method conflicts are caught", {
-  data <- gen_data(500, 4)
-  holdout <- gen_data(500, 4)
-  my_fit <- glmnet::cv.glmnet
-  my_fit_params <- list(nfolds = 5)
-  my_predict <- predict
-  my_predict_params <- list(s = "lambda.min")
-
-  expect_error(FLAME(data = data, holdout = holdout,
-                     PE_method = my_PE_cont,
-                     user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,
-                     user_PE_predict = my_predict,
-                     user_PE_predict_params = my_predict_params))
-})
